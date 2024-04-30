10c91d02bf4ab1364fe0dce70c7de20a8df51652
AdamGolan69 <slowmoon.adam@gmail.com>
2024-04-30 18:19:33 +0300

    Adding initiator

---
+++ initiator.mjs
@@ -0,0 +1,24 @@
+import { readFile, writeFile, unlinkSync } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Remove git files.
+unlinkSync(join(__dirname, '.gitmodules'));
+unlinkSync(join(__dirname, 'src/.git'));
+unlinkSync(join(__dirname, 'src/.gitignore'));
+unlinkSync(join(__dirname, 'src/.gitmodules'));
+unlinkSync(join(__dirname, 'src/style/.git'));
+unlinkSync(join(__dirname, 'src/style/.gitignore'));
+
+// Rewrite git config.
+readFile('./.git/config', { encoding: 'utf-8' }, (err, data) => {
+    err
+        ? console.log(err)
+        : writeFile('./.git/config', data.slice(data.indexOf(0, '[submodule')), { encoding: 'utf-8' });
+});
+
+// Remove this file.
+unlinkSync('./initiator.mjs');
